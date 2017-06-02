002e78254ccf3e401ab03e0082f11b695f6a5d19
Eric Carter <eric.carter@cryowatt.com>
2017-06-02 16:40:28 +0000

    A bunch of things working pretty ok

---
+++ Scripts/KubernetesDSC.ps1
@@ -0,0 +1,119 @@
+﻿#Requires -Version 5
+param($Ensure = "Present")
+
+Configuration KubernetesWorkerConf
+{
+    param($Ensure = "Present")
+
+    # Import the module that defines custom resources Import-DSCResource -ModuleName PSDesiredStateConfiguration
+    Import-DSCResource -ModuleName PackageManagementProviderResource
+    Import-DscResource –ModuleName PSDesiredStateConfiguration
+    Import-DscResource –ModuleName xPSDesiredStateConfiguration
+    Import-DscResource –ModuleName xHyper-V
+    Import-DSCResource -ModuleName KubernetesDSC
+    Import-DSCResource -ModuleName cChoco
+
+    Node Localhost
+    {
+        PackageManagementSource PSGallery
+        {
+            Name = "PSGallery"
+            ProviderName = "PowerShellGet"
+            InstallationPolicy = "Trusted"
+            SourceUri = "https://www.powershellgallery.com/api/v2/"
+        }
+
+        PSModule DockerMsftProvider
+        {
+            DependsOn = @("[PackageManagementSource]PSGallery")
+            Name = "DockerMsftProvider"
+            Repository = "PSGallery"
+            InstallationPolicy = "Trusted"
+        }
+
+        PackageManagementSource DockerPS
+        {
+            Name = "DockerPS-Dev"
+            ProviderName = "PowerShellGet"
+            InstallationPolicy = "Trusted"
+            SourceUri = "https://ci.appveyor.com/nuget/docker-powershell-dev"
+        }
+
+        PSModule DockerPS
+        {
+            DependsOn = @("[PackageManagementSource]DockerPS")
+            Name = "Docker"
+            Repository = "DockerPS-Dev"
+            InstallationPolicy = "Trusted"
+        }
+
+        PackageManagement docker
+        {
+            DependsOn = @("[PSModule]DockerMsftProvider")
+            Name = "docker"
+            ProviderName = "DockerMsftProvider"
+        }
+
+        Service docker
+        {
+            DependsOn = @("[PackageManagement]docker")
+            Name = "docker"
+            State = "Running"
+        }
+    
+        WindowsFeature Routing
+        {
+            Name = "Routing"
+        }
+
+        xVMSwitch KubeProxySwitch
+        {
+            Name = "KubeProxySwitch"
+            Type = "Internal"
+        }
+
+        DockerImage Pause
+        {
+            Name = "apprenda/pause"
+        }
+
+        cChocoInstaller installChoco
+        {
+            InstallDir = "c:\ProgramData\chocolatey"            
+        }
+
+        cChocoPackageInstaller kubernetes-node
+        {
+            Name = "kubernetes-node"
+            DependsOn = "[cChocoInstaller]installChoco"
+            Version = "1.7.0"
+        }
+    }
+}
+
+KubernetesWorkerConf
+Start-DSCConfiguration '.\KubernetesWorkerConf' -Wait -Force
+
+
+
+<#
+
+    Import-DSCResource -ModuleName PSDesiredStateConfiguration
+    Import-DSCResource -ModuleName PackageManagementProviderResource
+
+    PSModule DockerMsftProvider
+    {
+        Ensure = $Ensure
+        Name = "DockerMsftProvider"
+        Repository = "PSGallery"
+        InstallationPolicy = "Untrusted"
+    }
+
+    Service docker
+    {
+        DependsOn = @("[PSModule]DockerMsftProvider")
+        Name = "docker"
+        Ensure = $Ensure
+        State = "Running"
+    }
+#>
---
+++ Scripts/Prerequisites.ps1
@@ -0,0 +1,17 @@
+﻿#Requires -Version 5
+#Requires -Modules PackageManagementProviderResource
+<#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PackageManagementProviderResource -Repository PSGallery -Force
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force#>
+
+
+
+$requiredModules = @("PackageManagementProviderResource"; "PSDesiredStateConfiguration"; "xPSDesiredStateConfiguration"; "cChoco"; "Pscx"; "xHyper-V")
+$requiredModules | Where-Object { 
+    return (Get-InstalledModule -Name $_ -ErrorAction SilentlyContinue) -eq $null -and (Get-Module -Name $_) -eq $null
+} | ForEach-Object {
+    Install-Module -Name $_ -Force -AllowClobber
+}
+
+$requiredModules | ForEach-Object { Import-Module $_ }
