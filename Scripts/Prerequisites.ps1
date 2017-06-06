53bb6d52cc62cd5658ad1017a1ea1e9455295304
Cryowatt <eric.carter@cryowatt.com>
2017-06-06 22:06:39 +0000

    Adding missing prereq "Docker"

--- Scripts/Prerequisites.ps1
+++ Scripts/Prerequisites.ps1
@@ -1,13 +1,11 @@
 ﻿#Requires -Version 5
-#Requires -Modules PackageManagementProviderResource
+
 <#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PackageManagementProviderResource -Repository PSGallery -Force
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force#>
 
-
-
-$requiredModules = @("PackageManagementProviderResource"; "PSDesiredStateConfiguration"; "xPSDesiredStateConfiguration"; "cChoco"; "Pscx"; "xHyper-V")
+$requiredModules = @("PackageManagementProviderResource"; "PSDesiredStateConfiguration"; "xPSDesiredStateConfiguration"; "cChoco"; "Pscx"; "xHyper-V"; "Docker")
 $requiredModules | Where-Object { 
     return (Get-InstalledModule -Name $_ -ErrorAction SilentlyContinue) -eq $null -and (Get-Module -Name $_) -eq $null
 } | ForEach-Object {
