68f3f5d6c4269f0ec7b44154fde6048ce97865a5
Eric Carter <eric.carter@us.imshealth.com>
2017-06-12 11:12:39 -0700

    Added secret sauce to make all the dsc resources actually show up when importing the module.

---
+++ DSCResources/KubernetesNode/KubernetesNode.schema.psm1
@@ -0,0 +1,60 @@
+﻿Configuration KubernetesNode
+{
+    param(
+        [ipaddress]$HostName,
+        [string]$KubeConfig
+    )
+
+    Import-DSCResource -ModuleName cChoco
+
+    $chocoPath = "c:\ProgramData\chocolatey"
+    $kubeConfigPath = "C:\kube\kubeconfig"
+
+    Service docker
+    {
+        Name = "docker"
+        State = "Running"
+    }
+    
+    WindowsFeature Routing
+    {
+        Name = "Routing"
+    }
+
+    DockerImage Pause
+    {
+        Name = "apprenda/pause"
+    }
+
+    cChocoInstaller installChoco
+    {
+        InstallDir =  $chocoPath
+    }
+
+    cChocoPackageInstaller kubernetes-node
+    {
+        Name = "kubernetes-node"
+        DependsOn = "[cChocoInstaller]installChoco"
+        Version = "1.5.7"
+    }
+
+    File kubeconfig
+    {
+        DestinationPath = $kubeConfigPath
+        Contents = $KubeConfig
+    }
+
+    WindowsProcess kubelet
+    {
+        Arguments = "--kubeconfig $kubeConfigPath --require-kubeconfig --pod-infra-container-image='apprenda/pause' --hostname-override=$HostName"
+        Path = [System.IO.Path]::Combine($chocoPath, "bin\kubelet.exe")   
+        DependsOn = "[cChocoPackageInstaller]kubernetes-node", "[File]kubeconfig"
+    }
+
+    WindowsProcess kube-proxy
+    {
+        Arguments = "--kubeconfig $kubeConfigPath --hostname-override=$HostName --bind-address=$HostName --proxy-mode=userspace --v=3"
+        Path = [System.IO.Path]::Combine($chocoPath, "bin\kube-proxy.exe")   
+        DependsOn = "[cChocoPackageInstaller]kubernetes-node", "[File]kubeconfig"
+    }
+}
--- KubernetesDSC.psd1
+++ KubernetesDSC.psd1
@@ -9,10 +9,10 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = '.\KubernetesDSC.psm1'
+#RootModule = '.\KubernetesDSC.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.0.9'
+ModuleVersion = '0.0.10'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
@@ -66,7 +66,10 @@ RequiredModules = @('cChoco'; 'Docker'; 'DockerMsftProvider')
 # FormatsToProcess = @()
 
 # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
-# NestedModules = @()
+NestedModules = @(
+    'KubernetesDSC.psm1',
+    'DSCResources\DockerImage\DockerImage.psd1',
+    'DSCResources\KubernetesNode\KubernetesNode.psd1')
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 # FunctionsToExport = @()
@@ -81,10 +84,10 @@ RequiredModules = @('cChoco'; 'Docker'; 'DockerMsftProvider')
 AliasesToExport = @()
 
 # DSC resources to export from this module
-DscResourcesToExport = 'DockerImage, KubernetesNode'
+DscResourcesToExport = 'DockerImage', 'KubernetesNode'
 
 # List of all modules packaged with this module
-# ModuleList = @()
+# ModuleList = 'DockerImage', 'KubernetesNode'
 
 # List of all files packaged with this module
 # FileList = @()
