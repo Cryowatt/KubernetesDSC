2b9440db015c7444dc7594835cba351d969e9661
Eric Carter <eric.carter@us.imshealth.com>
2017-06-13 14:56:53 -0700

    Some Ensures default to absent???? WTF.

--- DSCResources/KubernetesNode/KubernetesNode.schema.psm1
+++ DSCResources/KubernetesNode/KubernetesNode.schema.psm1
@@ -12,17 +12,20 @@
 
     Service docker
     {
+        Ensure = "Present"
         Name = "docker"
         State = "Running"
     }
     
     WindowsFeature Routing
     {
+        Ensure = "Present"
         Name = "Routing"
     }
 
     DockerImage Pause
     {
+        Ensure = "Present"
         Name = "apprenda/pause"
     }
 
@@ -33,6 +36,7 @@
 
     cChocoPackageInstaller kubernetes-node
     {
+        Ensure = "Present"
         Name = "kubernetes-node"
         DependsOn = "[cChocoInstaller]installChoco"
         Version = "1.5.7"
@@ -40,12 +44,14 @@
 
     File kubeconfig
     {
+        Ensure = "Present"
         DestinationPath = $kubeConfigPath
         Contents = $KubeConfig
     }
 
     WindowsProcess kubelet
     {
+        Ensure = "Present"
         Arguments = "--kubeconfig $kubeConfigPath --require-kubeconfig --pod-infra-container-image='apprenda/pause' --hostname-override=$HostName"
         Path = [System.IO.Path]::Combine($chocoPath, "bin\kubelet.exe")   
         DependsOn = "[cChocoPackageInstaller]kubernetes-node", "[File]kubeconfig"
@@ -53,6 +59,7 @@
 
     WindowsProcess kube-proxy
     {
+        Ensure = "Present"
         Arguments = "--kubeconfig $kubeConfigPath --hostname-override=$HostName --bind-address=$HostName --proxy-mode=userspace --v=3"
         Path = [System.IO.Path]::Combine($chocoPath, "bin\kube-proxy.exe")   
         DependsOn = "[cChocoPackageInstaller]kubernetes-node", "[File]kubeconfig"
